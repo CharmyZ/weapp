0639eba4289657e37335ee233bc5925e3e83debf
Charmy <charmyzhang@163.com>
2017-03-31 09:40:02 +0800

    Update search_result.js

--- 微信小程序/pages/search/search_result.js
+++ 微信小程序/pages/search/search_result.js
@@ -5,63 +5,98 @@ Page({
     slideUrls: "",
     scaleWidth: "",
     scaleHeight: "",
+    title:"",
     hidden: true,
     currentPage: 0
   },
+   
   onLoad: function (options) {
-    
+    var that = this
     var rateNumber = options.id
-    var res = wx.getSystemInfoSync()
-    var windowWidth = res.windowWidth
-    var windowHeight = res.windowHeight
-    var imgUrla = "http://ytgrading.com/coin/request/casechart/pubFile/" + rateNumber.substr(0, 5) + "/" + rateNumber + "/" + "large" + "//" + rateNumber + "a" + ".jpg"
-    this.setData({
-      imgUrls: [imgUrla],
-      rateNumber: options.id,
-      slideDots: [],
-      indicatorDots: true,
-      autoplay: true,
-      interval: 7000,
-      duration: 500,
-      userInfo: {},
-      scaleHeight: windowHeight,
-      scaleWidth: windowWidth
+    var phone = wx.getSystemInfoSync()
+    var windowWidth = phone.windowWidth
+    var windowHeight = phone.windowHeight
+
+    wx.request({
+      url: 'https://qc.ytgrading.com/de/searchImagesData.shtml',
+      data: { rateNumber },
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        var obj = res.data.data
+        var img1 = obj.path1
+        var img2 = obj.path2
+        var title = obj.title
+        console.log(res.data.data)
+        that.setData({
+          imgUrls: [img1, img2],
+          rateNumber: options.id,
+          slideDots: [],
+          title : title,
+          indicatorDots: true,
+          autoplay: true,
+          interval: 7000,
+          duration: 500,
+          userInfo: {},
+          scaleHeight: windowHeight,
+          scaleWidth: windowWidth
+        })
+
+        /* if (img1 = null) {
+          that.setData({
+            hidden: false,
+          });
+        } else {
+          that.setData({
+            hidden: true,
+          })
+        }
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }*/
+       var imgUrls = that.data.imgUrls
+     var currentUrl = imgUrls[imgUrls.length - 1]
+    
+     if (currentUrl == "") {
+       that.setData({
+         hidden: false,
+       });
+     } else {
+       that.setData({
+         hidden: true,
+       })
+ 
+      }
+      }
     })
-
+    
+   
+    
   },
+   imgerror: function (e) {
+    
+   },
 
-  imgerror: function (e) {
-    var imgUrls = this.data.imgUrls
-    var currentUrl = imgUrls[imgUrls.length - 1]
-    imgUrls.pop();
-    this.setData({
-      imgUrls: imgUrls,
-    });
-    if (imgUrls.length == 0) {
-      this.setData({
-        hidden: false,
-      });
-    } else {
-      this.setData({
-        hidden: true,
-      });
-
-    }
-  },
-  imageLoad: function (e) {
-    var imgUrls = this.data.imgUrls
-    var currentUrl = imgUrls[imgUrls.length - 1]
-    var keyChar = currentUrl.charAt(currentUrl.length - 5)
-    var nextChar = String.fromCharCode(keyChar.charCodeAt() + 1)
-    var nextUrl = currentUrl.substring(0, currentUrl.length - 5) + nextChar + currentUrl.substr(currentUrl.length - 4)
-    imgUrls.push(nextUrl);
-    this.setData({
-      imgUrls: imgUrls
-    })
-
-  },
+   /*imageLoad: function (e) {
+     var imgUrls = this.data.imgUrls
+     var currentUrl = imgUrls[imgUrls.length - 1]
+     var keyChar = currentUrl.charAt(currentUrl.length - 5)
+     var nextChar = String.fromCharCode(keyChar.charCodeAt() + 1)
+     var nextUrl = currentUrl.substring(0, currentUrl.length - 5) + nextChar + currentUrl.substr(currentUrl.length - 4)
+     imgUrls.push(nextUrl);
+     this.setData({
+       imgUrls: imgUrls
+     })
+ 
+   },*/
   swiperChange: function (event) {
-    var tem = event.detail.current
+    var tem = event.detail.current  //此时切换到第几张
     this.setData({
       currentPage: tem
     });
@@ -70,12 +105,12 @@ Page({
   clickImg: function (e) {
     var current = this.data.currentPage
     var imgUrls = this.data.imgUrls
-    var clickUrl = imgUrls[current]
+    var clickUrl = imgUrls[current] //获取点击图片的url
     wx.navigateTo({
       url: '/pages/search/search_result_oneimg?id=' + clickUrl
     })
   },
-  
+
 
 })
 
